4646582ed71f79e3cd46d820cd26f10db768ee63
Kimba FONDIO <foundkim@live.fr>
2025-05-14 15:29:27 +0200

    Project Initialization

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCs5DQXCF6ouOYh4FPeaV1sTEKT2ugi7oQ',
+    authDomain: 'zechlab-dev.firebaseapp.com',
+    projectId: 'zechlab-dev',
+    storageBucket: 'zechlab-dev.firebasestorage.app',
+    messagingSenderId: '792761969874',
+    appId: '1:792761969874:web:8efe9bb574d99d7ad3910b',
+    measurementId: 'G-4Y3TQDZ4QZ',
+  },
+};
