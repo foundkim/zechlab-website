82c436e245abf421781caa28e0a11723302873e8
Dak2dak <dazibao76@gmail.com>
2025-07-03 06:48:23 +0200

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PresentationComponent } from './features/about/pages/presentation/presentation.component';
+import { TeamComponent } from './features/about/pages/team/team.component';
+import { ValuesComponent } from './features/about/pages/values/values.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'presentation', component: PresentationComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'values', component: ValuesComponent },
+];
