df10d57c004b2483ff64cfe710e3965265be9785
Dak2dak <dazibao76@gmail.com>
2025-07-04 19:51:57 +0200

    adding presentation and team component content

--- src/app/features/about/module/about.module.ts
+++ src/app/features/about/module/about.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { PresentationComponent } from '../pages/presentation/presentation.component';
 import { TeamComponent } from '../pages/team/team.component';
 import { ValuesComponent } from '../pages/values/values.component';
+import { ServiceComponent } from '../pages/service/service.component';
+
 
 @NgModule({
   declarations: [],
@@ -14,6 +16,7 @@ import { ValuesComponent } from '../pages/values/values.component';
       { path: 'presentation', component: PresentationComponent },
       { path: 'team', component: TeamComponent },
       { path: 'values', component: ValuesComponent },
+      {path: 'service', component: ServiceComponent},
     ]),
   ],
 })
---
+++ src/app/features/about/pages/team-card/team-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { SharedModule } from '../../../../shared/shared.module';
+
+@Component({
+  selector: 'app-team-card',
+  imports: [SharedModule],
+  templateUrl: './team-card.component.html',
+  styleUrl: './team-card.component.css'
+})
+export class TeamCardComponent {
+  @Input() title!: string;
+  @Input() addressLines!: string[];
+  @Input() phone!: string;
+  @Input() photo!: string;
+  @Input() actions!: { icon: string; label: string; link: string }[];
+}
